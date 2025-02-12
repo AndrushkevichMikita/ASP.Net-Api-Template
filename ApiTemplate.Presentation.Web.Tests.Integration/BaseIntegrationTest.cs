2a86fcd0f5486cf485e154184d0f08daef7374cc
Mikita Andrushkevich <Mikita.Andrushkevich@ventionteams.com>
2025-02-12 22:53:51 +0200

    Tests. Integration. Atempt to fix docker setup in github actions #2

--- ApiTemplate.Presentation.Web.Tests.Integration/BaseIntegrationTest.cs
+++ ApiTemplate.Presentation.Web.Tests.Integration/BaseIntegrationTest.cs
@@ -1,5 +1,4 @@
 ﻿using ApiTemplate.Infrastructure;
-using DotNet.Testcontainers.Builders;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -13,9 +12,10 @@ namespace ApiTemplate.Presentation.Web.Tests.Integration
     {
         private readonly MsSqlContainer _mssqlContainer = new MsSqlBuilder()
                                                              .WithCleanUp(true)
-                                                             .WithPortBinding(1433)
-                                                             .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(1433))
+                                                             .WithImage("mcr.microsoft.com/mssql/server:2017-latest-ubuntu")
+                                                             .WithPortBinding(1433, true) // Bind a random available port
                                                              .Build();
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureTestServices(services =>
